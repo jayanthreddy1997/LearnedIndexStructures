b90af1b70cb0f747d710de0a28dc3b5d9a41c84a
Jayanth Reddy <njkreddy1997@gmail.com>
2023-12-21 12:11:12 -0500

    add tests

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # LearnedIndexStructures
 
-## Commands
-- Run Training: `python main.py`
-- Run inference performance tests: `python inference_time_test.py`
+## Project Description
+TODO
+
+## Project milestones
+TODO
+
+## Description of the repository and code structure
+TODO
+
+## Dependencies
+TODO
+
+## Commands to run experiments
+- Run training for all datasets: `python main.py`
+- Run inference performance tests: `python inference_time_tests.py`
+- Run R-squared and memory stats tests: `python tests.py`
+
+## Results
+TODO
+
--- tests.py
+++ tests.py
@@ -1,9 +1,13 @@
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from dataloader import get_dataloader
+from utils import get_config
+from learned_index import LearnedIndex
 
 
-def test1():
+def test_data():
     data_fp = f'/Users/reddyj/Desktop/workspace/nyu/courses/idls/project/SOSD/data/normal_200M_uint32'
     with open(data_fp, 'rb') as f:
         packed_data = f.read()
@@ -12,7 +16,8 @@ def test1():
     plt.hist(data[200000000-1000000:], bins=100)
     plt.show()
 
-def test2():
+
+def test_torch_compile():
     import torch
 
     torch.set_default_device("cuda:0")
@@ -22,6 +27,51 @@ def test2():
 
     a = torch.zeros(100)
     print(test_fn(a))
-    
+
+
+def test_model_perf():
+    """
+    Computes the R-squared performance metric
+    """
+    overall_tss = 0.0
+    overall_rss = 0.0
+    for dataset in ['norm', 'logn', 'uspr']:
+        config = get_config(f'conf/hpc_config_{dataset}.yml')
+        dataloader = get_dataloader(config['data_path'], batch_size=100000)
+        learned_index = LearnedIndex(config['weights_fp'])
+
+        for inputs, targets in dataloader:
+            predicted_values, _ = learned_index.get_predictions(inputs)
+
+            mean_targets = torch.mean(targets)
+            tss_minibatch = torch.sum((targets - mean_targets) ** 2)
+
+            # Calculate Residual Sum of Squares (RSS) for the minibatch
+            rss_minibatch = torch.sum((targets - predicted_values) ** 2)
+
+            # Accumulate TSS and RSS
+            overall_tss += tss_minibatch.item()
+            overall_rss += rss_minibatch.item()
+        # Compute overall R-squared
+        overall_r_squared = 1 - (overall_rss / overall_tss)
+        print(f"Dataset: {dataset}, Overall R-squared: {overall_r_squared}")
+
+
+def test_memory_usage():
+    for dataset in ['norm', 'logn', 'uspr']:
+        config = get_config(f'conf/hpc_config_{dataset}.yml')
+        dataloader = get_dataloader(config['data_path'], batch_size=100000)
+        learned_index = LearnedIndex(config['weights_fp'])
+
+        keys, locations = next(iter(dataloader))
+        device = torch.device('cuda')
+        for i in range(5):  # warmup
+            predictions, time_taken = learned_index.get_predictions(keys)
+        torch.cuda.reset_peak_memory_stats(device=device)
+        temp1, temp2 = learned_index.get_predictions(keys)
+        print(f"Dataset: {dataset} | Peak memory consumption: {torch.cuda.max_memory_allocated(device=device)}")
+
+
 if __name__ == '__main__':
-    test2()
+    test_model_perf()
+    test_memory_usage()
